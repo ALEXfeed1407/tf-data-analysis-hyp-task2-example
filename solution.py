3fc8235701935f155f192e9d4b77a5368fefb9e5
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-12 14:51:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from scipy.stats import ks_2samp
+    from hyppo.ksample import MMD
     alpha = 0.07
-    stat1, p1 = ks_2samp(x, y)
+    stat1, p1 = MMD().test(np.array(x), np.array(y))
     return p1 < alpha # Ваш ответ, True или False
