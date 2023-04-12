b5bdf7609d5bd34d7a26091ad45f606f84874a99
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-12 15:02:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from hyppo.ksample import MMD
+    from hyppo.ksample import Energy
     alpha = 0.07
-    stat1, p1 = MMD().test(np.array(x), np.array(y))
+    stat1, p1 = Energy().test(np.array(x), np.array(y))
     return p1 < alpha # Ваш ответ, True или False
