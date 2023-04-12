c99180b1bf2537631e6ab5c33898b1ba182fd83b
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-12 22:06:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from hyppo.ksample import Energy
+    import scipy.stats as st
     alpha = 0.07
-    stat1, p1 = Energy().test(np.array(x), np.array(y))
-    return p1 < alpha # Ваш ответ, True или False
+    a = st.anderson_ksamp([x, y]).pvalue
+    return a < alpha
